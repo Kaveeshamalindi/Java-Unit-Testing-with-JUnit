1f9af3a0e2bb37971bc7fb0080dd3f03c5be6872
Kaveesha Senarathne <160614811+Kaveeshamalindi@users.noreply.github.com>
2026-06-08 08:41:49 +0530

    Add files via upload

---
+++ Calculator/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public double divide (int a, int b){
+        if (b == 0){
+            throw new IllegalArgumentException("Cannot divide by Zero");
+        }
+        return (double) a/b;
+    }
+}
